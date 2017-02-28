14a4507bf6edcb755aaf24ebdfea70cac78d82fe
Felipe Pozzibon <felipepozzi@hotmail.com>
2017-02-27 22:26:19 -0300

    oi

--- app/routes/promocoes.js
+++ app/routes/promocoes.js
@@ -1,4 +1,4 @@
-modeule.exports = function(app){
+module.exports = function(app){
     app.get('/produtos', function(req,res,next){
       var connection = app.infra.connectionFactory();
       var ProdutosDAO = new app.infra.ProdutosDAO(connection);
@@ -9,7 +9,7 @@ modeule.exports = function(app){
 
     app.post("/promocoes", function(req,res){
       var promocao = req.body;
-      io.emit('novaPromocao', promocao);
+      app.get('io').emit('novaPromocao', promocao);
       res.redirect('promocoes/form');
     });
 }
